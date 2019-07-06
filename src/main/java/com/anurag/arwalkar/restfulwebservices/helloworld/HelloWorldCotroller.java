6a087675adf41b7e1b5111b50d0cd6ea26679a92
anurag arwalkar <anuragarwalkar@gmail.com>
2019-07-06 11:15:25 +0530

    cross origin

--- src/main/java/com/anurag/arwalkar/restfulwebservices/helloworld/HelloWorldCotroller.java
+++ src/main/java/com/anurag/arwalkar/restfulwebservices/helloworld/HelloWorldCotroller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4300")
+@CrossOrigin
 @RestController
 public class HelloWorldCotroller {
 	
